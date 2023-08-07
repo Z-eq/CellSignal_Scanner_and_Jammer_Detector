c6affe80dcdfbe7a197e46f3acc191154942b897
Z-eq <106398287+Z-eq@users.noreply.github.com>
2023-08-07 20:11:56 +0000

    Update script.js
    add redundant socket event handling removed and the code streamlined instead for better better performance

--- static/script.js
+++ static/script.js
@@ -25,8 +25,6 @@ window.onload = function () {
         });
     }
 
-    // ... [Rest of the existing code from the second script]
-
     const socket = io();
 
     // Connect to the server
@@ -34,33 +32,21 @@ window.onload = function () {
 
     // Listen for the 'continuous_update' event from the server
     socket.on('continuous_update', data => {
-        // Update the UI with current signal strength and moving average
         const currentStrengthDisplay = document.getElementById('currentStrength');
         const movingAvgDisplay = document.getElementById('movingAvg');
 
-        currentStrengthDisplay.innerText = `Current Strength: ${data.current_strength}`;
-        movingAvgDisplay.innerText = `Moving Average: ${data.moving_avg}`;
+        currentStrengthDisplay.innerText = `Current Strength: ${data.current_strength} dBm`;
+        movingAvgDisplay.innerText = `Moving Average: ${data.moving_avg} dBm`;
     });
 
     // Listen for the 'jamming_detected' event from the server
     socket.on('jamming_detected', data => {
-        // Update the UI to indicate jamming detected
         const jammingStatus = document.getElementById('jamming-alert');
         jammingStatus.innerText = 'Jamming Detected!';
         jammingStatus.style.color = 'red';
         jammingStatus.style.fontWeight = 'bold';
 
-        // Add the new jamming event to the table
         addJammingEventToTable(data);
-    });
-
-    // Listen for the 'reset_status' event from the server
-    socket.on('reset_status', () => {
-        // Update the UI to indicate status reset
-        const jammingStatus = document.getElementById('jamming-alert');
-        jammingStatus.innerText = 'OK';
-        jammingStatus.style.color = 'black';
-        jammingStatus.style.fontWeight = 'normal';
     });
 
     // Function to add a jamming event to the table
@@ -70,7 +56,21 @@ window.onload = function () {
         newRow.insertCell(0).innerText = new Date(data.timestamp * 1000).toLocaleString();
         newRow.insertCell(1).innerText = data.current_strength;
         newRow.insertCell(2).innerText = data.moving_avg;
-        newRow.insertCell(3).innerText = data.packet_loss_avg;
+        newRow.insertCell(3).innerText = data.packet_loss_avg || 'N/A';
+
+    }
+
+    // Function to fetch the latest signal data from the server
+    function fetchLatestSignalData() {
+        fetch('/get_latest_signal_data')
+            .then(response => response.json())
+            .then(data => {
+                const currentStrengthDisplay = document.getElementById('currentStrength');
+                const movingAvgDisplay = document.getElementById('movingAvg');
+
+                currentStrengthDisplay.innerText = `Current Strength: ${data.current_strength} dBm`;
+                movingAvgDisplay.innerText = `Moving Average: ${data.moving_avg} dBm`;
+            });
     }
 
     // Function to fetch latest jamming events from the server
@@ -78,18 +78,17 @@ window.onload = function () {
         fetch('/get_latest_jamming_events')
             .then(response => response.json())
             .then(data => {
-                // Clear the table
                 const tableBody = document.getElementById('latestEventsTableBody');
                 tableBody.innerHTML = '';
-
-                // Add each jamming event to the table
                 data.forEach(event => {
                     addJammingEventToTable(event);
                 });
             });
     }
 
-    // Fetch latest jamming events every 5 seconds
-    setInterval(fetchLatestJammingEvents, 5000);
+    // Fetch the latest signal data and jamming events every 5 seconds
+    setInterval(() => {
+        fetchLatestSignalData();
+        fetchLatestJammingEvents();
+    }, 1000);
 };
-
